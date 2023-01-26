b57d2ca7c9ac7d86b7addd4bb471f106533d3a91
Yair-f <120482613+Yair-f@users.noreply.github.com>
2023-01-26 08:34:11 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,12 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 // Don't forget to comment your code as you work!
 let notPlanning: Sprite = null
 let planning: Sprite = null
 scene.setBackgroundColor(6)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . b 5 5 b . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . b b b b b 5 5 5 5 5 5 5 b . . 
-    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-    . . b d 5 5 b 1 f f 5 4 4 c . . 
-    b b d b 5 5 5 d f b 4 4 4 4 b . 
-    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Player)
 let birthday = game.askForNumber("How many days are left till your birthday?", 4)
 let name = game.askForString("What's your name?", 12)
 if (birthday < 20) {
@@ -39,21 +26,39 @@ if (birthday < 20) {
         . . . f 5 f f f 5 f f 5 f . . . 
         . . . f f . . f f . . f f . . . 
         `, SpriteKind.Player)
+    for (let index = 0; index < 3; index++) {
+        planning.sayText("It's almost my birthday!")
+    }
+    animation.runMovementAnimation(
+    planning,
+    animation.animationPresets(animation.bobbing),
+    2000,
+    true
+    )
 } else {
     notPlanning = sprites.create(img`
-        . . 4 4 4 . . . . 4 4 4 . . . . 
-        . 4 5 5 5 e . . e 5 5 5 4 . . . 
-        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-        . e e 5 5 5 5 5 5 5 5 e e . . . 
-        . . e 5 f 5 5 5 5 f 5 e . . . . 
-        . . f 5 5 5 4 4 5 5 5 f . . f f 
-        . . f 4 5 5 f f 5 5 6 f . f 5 f 
-        . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
-        . . . f 4 5 5 5 5 5 5 4 4 5 f . 
-        . . . f 5 5 5 5 5 4 5 5 f f . . 
-        . . . f 5 f f f 5 f f 5 f . . . 
-        . . . f f . . f f . . f f . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
         `, SpriteKind.Player)
+    for (let index = 0; index < 3; index++) {
+        notPlanning.sayText("It's still not my birthday!")
+    }
+    animation.runMovementAnimation(
+    notPlanning,
+    animation.animationPresets(animation.shake),
+    2000,
+    true
+    )
 }
